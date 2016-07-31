173d1aafb78af0f06cc12938fb71bf9774847a88
Michael Gardener <mgardener@cissq.com>
2016-07-30 23:54:42 -0400

    Reopened submission issue

--- lib.php
+++ lib.php
@@ -613,7 +613,8 @@ function block_ned_mentor_assignment_status($mod, $userid) {
 
             $submissionisgraded = reset($submissionisgraded);
             if ($submissionisgraded->grade > -1) {
-                if ($submission->timemodified > $submissionisgraded->timemodified) {
+                if (($submission->timemodified > $submissionisgraded->timemodified)
+                    || ($submission->attemptnumber > $submissionisgraded->attemptnumber)) {
                     $graded = false;
                 } else {
                     $graded = true;
@@ -633,11 +634,7 @@ function block_ned_mentor_assignment_status($mod, $userid) {
             }
         }
         if ($submission->status == 'reopened') {
-            if ($graded) {
-                return 'submitted';
-            } else {
-                return 'waitinggrade';
-            }
+            return 'submitted';
         }
         if ($submission->status == 'submitted') {
             if ($graded) {
