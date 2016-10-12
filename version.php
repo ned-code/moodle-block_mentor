d73b2c17560a1993cfb3893a41c42f113b5c1a16
NED <fernandooliveira@knet.ca>
2016-10-12 10:13:40 -0400

    Update version.php

--- version.php
+++ version.php
@@ -29,5 +29,5 @@ $plugin->version   = 2016101102;
 $plugin->requires  = 2013051400;
 $plugin->component = 'block_fn_mentor';
 $plugin->cron = 30;
-$plugin->release = '3.1.4';
+$plugin->release = '3.1.5';
 $plugin->maturity = MATURITY_STABLE;
