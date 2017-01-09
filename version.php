5ab33526354d7426180119fc5691a9ad2abd91ee
NED <fernandooliveira@knet.ca>
2017-01-09 10:07:45 -0500

    Update version.php

--- version.php
+++ version.php
@@ -29,5 +29,5 @@ $plugin->version   = 2017010800;
 $plugin->requires  = 2013051400;
 $plugin->component = 'block_fn_mentor';
 $plugin->cron = 30;
-$plugin->release = '3.1.8';
+$plugin->release = '3.1.9';
 $plugin->maturity = MATURITY_STABLE;
